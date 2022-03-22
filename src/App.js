b8d4295476d78919e96945bd94467a53ce6dc7d6
Vignesh <vignesh@absoluteapplabs.in>
2022-03-22 14:32:26 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [counter, setCounter] = useState(0);
   return (
-    <div className="App">
-        Home 
+    <div className="App"><br></br>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter(counter - 1)}>-</button>
+
     </div>
   );
 }
